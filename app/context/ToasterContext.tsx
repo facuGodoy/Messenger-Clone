749923bc658c4f49bd8db384a6c4c8d7a8e211d5
Facundo N Godoy <facu14_23@hotmail.com>
2023-05-23 00:34:31 -0300

    changes in app

---
+++ app/context/ToasterContext.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import { describe } from "node:test";
+import { Toaster } from "react-hot-toast";
+
+const ToasterContext = () => { 
+  return (
+    <Toaster/>
+  )
+}
+
+export default ToasterContext;
