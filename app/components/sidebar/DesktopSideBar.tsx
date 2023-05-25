b440bd7a6b1a344a6d949b1400f4ae17f031b1bd
Facundo N Godoy <facu14_23@hotmail.com>
2023-05-24 22:34:34 -0300

    "changes SideBar mobile and navigate"

--- app/components/sidebar/DesktopSideBar.tsx
+++ app/components/sidebar/DesktopSideBar.tsx
@@ -9,11 +9,26 @@ const DesktopSideBar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div className="hidden lg:fixed lg:inset-y-0 lg:left-0 lg:z-40 lg:w-20 xl:px-6 lg:overflow-y-auto lg:bg-white lg:border-r-[1px] lg:pb-4 lg:flex lg:flex-col justify-between">
+    <div
+      className="
+    hidden
+    lg:fixed 
+    lg:inset-y-0 
+    lg:left-0 
+    lg:z-40 
+    lg:w-20 
+    xl:px-6 
+    lg:overflow-y-auto 
+    lg:bg-white 
+    lg:border-r-[1px] 
+    lg:pb-4 
+    lg:flex 
+    lg:flex-col 
+    justify-between"
+    >
       <nav className="mt-4 flex flex-col justify-between">
         <ul role="list" className="flex flex-col items-center space-y-1">
-          SideBar
-          {/* {routes.map((item) => (
+          {routes.map((item) => (
             <DesktopItem
               key={item.label}
               href={item.href}
@@ -22,7 +37,7 @@ const DesktopSideBar = () => {
               active={item.active}
               onClick={item.onClick}
             />
-          ))} */}
+          ))}
         </ul>
       </nav>
     </div>
--- app/components/sidebar/SideBar.tsx
+++ app/components/sidebar/SideBar.tsx
@@ -1,8 +1,10 @@
 import DesktopSideBar from "./DesktopSideBar";
+import MobileFooter from "./MobileFooter";
 
 async function SideBar({ children }: { children: React.ReactNode }) {
   return (
     <div className="h-full">
+      <MobileFooter />
       <DesktopSideBar />
       <main className="lg:pl-20 h-full">{children}</main>
     </div>
