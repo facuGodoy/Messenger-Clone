69496caddf9606ff1bcb6f38115ac17c2c89f592
Facundo N Godoy <facu14_23@hotmail.com>
2023-05-25 21:12:12 -0300

    "Finish SideBar, Navigation and Avatar"

--- app/components/sidebar/SideBar.tsx
+++ app/components/sidebar/SideBar.tsx
@@ -1,11 +1,15 @@
+import getCurrentUser from "../../actions/getCurrentUser";
+
 import DesktopSideBar from "./DesktopSideBar";
 import MobileFooter from "./MobileFooter";
 
 async function SideBar({ children }: { children: React.ReactNode }) {
+  const currentUser = await getCurrentUser();
+
   return (
     <div className="h-full">
       <MobileFooter />
-      <DesktopSideBar />
+      <DesktopSideBar currentUser={currentUser!} />
       <main className="lg:pl-20 h-full">{children}</main>
     </div>
   );
